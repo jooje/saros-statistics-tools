9ec747fe7004ebfcc0a9981bdcaf243c41288569
flthiel@FU-BERLIN.DE <flthiel@FU-BERLIN.DE@ac914741-dbed-0310-af48-c52ae7def1c7>
2010-06-14 18:37:30 +0000

    update and fix statistics runner shell script (dangling variables)
    git-svn-id: https://svn.imp.fu-berlin.de/agse/students/von_hoffen/statProcStandAlone@7385 ac914741-dbed-0310-af48-c52ae7def1c7

--- update-statistic-plots.sh
+++ update-statistic-plots.sh
@@ -1,17 +1,25 @@
 #! /bin/sh
 
-SOURCE_DATA_DIR="/home/saros-statistics/data/saros-statistic"
-AGGREGATED_DIR="/home/saros-statistics/statistics-aggregated"
-OUTPUT_DIR="/home/saros-statistics/statistics-plots"
+# png output is hard-coded in "plots" subdirectory of current dir
+
+BASE_DIR="/home/saros-statistics"
+SOURCE_DATA_DIR="$BASE_DIR/data/saros-statistic"
+AGGREGATED_DIR="$BASE_DIR/statistics-aggregated"
+SCRIPT_DIR="$BASE_DIR/statProcStandAlone"
 
 # this is hard-coded in StatAggregator
 AGGREGATED_FILENAME="data.txt"
 
+set -e
+
+cd $BASE_DIR
 
 mkdir -p $AGGREGATED_DIR
 rm -f $AGGREGATED_DIR/$AGGREGATED_FILENAME
 
-java -jar StatAggregator.jar $SOURCE_DATA_DIR $AGGREGATED_DIR
-rm -f $OUTPUT_DIR/*
+java -jar $SCRIPT_DIR/StatAggregator.jar $SOURCE_DATA_DIR $AGGREGATED_DIR
 
-Rscript --vanilla Analysis.R $AGGREGATED_DIR/$AGGREGATED_FILENAME
+# plots dir is hard-coded in StatAggregator
+rm -f $BASE_DIR/plots/*
+
+Rscript --vanilla $SCRIPT_DIR/Analysis.R $AGGREGATED_DIR/$AGGREGATED_FILENAME
